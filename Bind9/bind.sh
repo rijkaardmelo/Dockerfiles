3d6877d166143e109f2c1b52593cd15d23fb5c9f
Rijkaard Melo <rijkaardmelo@gmail.com>
2022-05-01 13:53:21 -0300

    main

---
+++ Bind9/bind.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+useradd -m administrador
+
+echo "administrador:administrador" | chpasswd
+
+passwd --expire administrador
+
+echo "root:root" | chpasswd
+
+passwd --expire root
+
+chmod 755 /etc/bind/named.conf.log
+
+mkdir /var/log/bind/
+
+cat > /var/log/bind/bind.log
+
+chown bind:bind /var/log/bind/
+
+/etc/init.d/bind9 start
+
+service ssh start
+
+tail -f /var/log/bind/bind.log
--- Bind9/bind9.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/etc/init.d/bind9 start
-
-tail -f /var/log/bind9/bind9.log
